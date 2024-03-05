1ad7be89f4665104a1ea2d31bd0baa3907a7a50c
Jesus61302 <jesushernandez61302@gmail.com>
2024-03-05 16:56:37 -0600

    made upgrades to dashboard styling and functionality

--- WebApplication/frontend/apws/src/components/Dashboard.js
+++ WebApplication/frontend/apws/src/components/Dashboard.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import "../styles/dashboard.css";
-import { BrowserRouter as Routes, Route, useNavigate, Link, Navigate} from "react-router-dom";
 import Plant from './Plant';
 
-
-
-
-
 function Dashboard(){
+  //might need to use  useRef instead of useState if rendering problems occur with useEffect
+  const[plants, setPlants] = useState([["plant 1" , 0, 1, 2, 3, 4], ["plant 2" , 0, 11, 22, 33, 44]]);
+
+  useEffect(() =>{
+    //this is the fetch from create user, will uncomment and make necceesarry changes once backend is complete
+    // fetch("http://127.0.0.1:5000/create_user", {
+    //     method: 'POST',
+    //     mode: "cors",
+    //     headers:{
+    //       'content-Type':'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       username: u,
+    //       password: p,
+    //     })
+    //   })
+    
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log("What's going on")
+    //     console.log(data['message'])
+    //     if (data['message'] === 'User added.') {
+    //       this.setState({
+    //         success: true,
+    //       })}
+    //   })
+  },[]);//empty array so this only runs when component initially mounts
 
   return(
     <div className='dashboard'>
@@ -15,21 +37,26 @@ function Dashboard(){
         <h1 className='dash-title'>APWS</h1>
         <a href='/'>
           <button className='head-btn'>Sign Out</button>
-          <button className='head-btn'>Settings</button>
         </a> 
+        <button className='head-btn'>Settings</button>
       </header>
       <div className='dash-display'>
-        <button id='joinBtn'>Join</button>
-        <div>
-          <Plant></Plant>
-          <Plant></Plant>
-          <Plant></Plant>
-          <Plant></Plant>
-        </div> 
-        
+        <button className='joinBtn'>Join</button>
+        <ul className='plant-list'>
+          {plants.map((plant, index) =>
+            <li key={index}>
+              <Plant name={plant[0]}
+                      moistL={plant[1]}
+                      tempL={plant[2]}
+                      humidL={plant[3]}
+                      lightL={plant[4]}
+                      alerts={plant[5]} />
+            </li>
+          )}
+        </ul>
       </div>
       <footer className='dash-foot'>
-        <p>&copy; APWS {new Date().getFullYear()}</p>
+        <p className='copyright'>&copy; APWS {new Date().getFullYear()}</p>
       </footer>
     </div>
   );
--- WebApplication/frontend/apws/src/components/Plant.jsx
+++ WebApplication/frontend/apws/src/components/Plant.jsx
@@ -1,22 +1,44 @@
 import plantpic from '../styles/plantpic.jpg';
 import '../styles/Plant.css';
+import PropTypes from 'prop-types';
 
 
-function Plant(){
+function Plant(props){
+
 
     return(
         <div className='plantCard'>
             <img className='img' src={plantpic} ></img>
-            <h2 className='card-title'>Plant Name</h2>
-            <p className='card-text'>Moisture Level: n/a  </p>
-            <p className='card-text'>Temperature Level: n/a  </p>
-            <p className='card-text'>Humidity Level: n/a  </p>
-            <p className='card-text'>Light level: n/a</p>
-            <p className='card-text'>Alerts : n/a</p>
+            <h2 className='card-title'>{props.name}</h2>
+            <p className='card-text'>Moisture Level: {props.moistL}  </p>
+            <p className='card-text'>Temperature Level: {props.tempL}  </p>
+            <p className='card-text'>Humidity Level: {props.humidL}  </p>
+            <p className='card-text'>Light level: {props.lightL}</p>
+            <p className='card-text'>Alerts : {props.alerts}</p>
             <button className='history-btn' >History</button>
             <br></br>
             <button className='plant-settings-btn'>plant settings</button>
         </div>
     )
 }
+
+Plant.propTypes = {
+    name: PropTypes.string,
+    moistL: PropTypes.number,
+    tempL: PropTypes.number,
+    humidL: PropTypes.number,
+    lightL: PropTypes.number,
+    alerts: PropTypes.string,
+}
+
+Plant.defaultProps = {
+    name: "Plant Name",
+    moistL: "n/a",
+    tempL: "n/a",
+    humidL: "n/a",
+    lightL: "n/a",
+    alerts: "n/a",
+
+}
+
 export default Plant
