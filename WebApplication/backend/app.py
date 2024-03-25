527a126d6ebd7f78da6ee2735e9f33bfd59c422a
Kiet Le <Kietle24@utexas.edu>
2024-03-25 09:38:21 -0500

    Updated app.py

--- WebApplication/backend/app.py
+++ WebApplication/backend/app.py
@@ -34,9 +34,9 @@ def login_page():
 #create_user route
 @app.route("/create_user", methods=['POST'])
 def create_user():
-    print("REACHED")
-    print(request)
-    print(client)
+    # print("REACHED")
+    # print(request)
+    # print(client)
     response = ''
     response = LoginFunctions.sign_up(request,client)
     print(response)
