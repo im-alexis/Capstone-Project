0dab101cfd370ae2c45024221f06061c875eba1e
Kiet Le <Kietle24@utexas.edu>
2024-03-31 02:43:54 -0500

    Sending Emails is Partially Setup

--- WebApplication/backend/MessageFunctions.py
+++ WebApplication/backend/MessageFunctions.py
@@ -1,5 +1,3 @@
-import smtplib
-
 # IF YOU WANT TO RUN, IT WILL ONLY SEND IN MAILTRAP'S TESTING SITE #
 
 #---------------------------------------#
@@ -10,33 +8,59 @@ import smtplib
 #---------------------------------------#
 
 #---------------------------------------#
-#           MailTrap Login              #
-#---------------------------------------#
-#                                       #
-#          Use APWS Gmail Login         #
-#                                       #
-#---------------------------------------#
-
-#---------------------------------------#
 #           Example Data                #
 #---------------------------------------#
 # sender = "apws.services@gmail.com"    #
-# receiver = "Kietvle2020@gmail.com"    #
+# receiver = "Kietle24@utexas.com"      #
 # subject = "Your Water Tank is Low"    #
-# Body = "(Detailed Report)"            #
+# Body = f"{Detailed Report}"           #
 #---------------------------------------#
 
+import smtplib
+from email.mime.text import MIMEText
+
+subject = "Testing APWS Email System"
+body = f"Now This...THIS is Epic\n\n\n\n\n"
 sender = "apws.services@gmail.com"
+recipient = "Kietvle2020@gmail.com"
 
+battery = f"""\
+Alert: Battery of Probe {pnum} of System {systemID} needs to be replaced soon.
 
-def send_email(receiver, subject, body):
-    message = f"""\
-    Subject: {subject}
-    To: {receiver}
-    From: {sender}
+Current Battery Life: {life}."""
 
-    {body}"""
+Hub = f"""\
+Alert: Battery of the Hub of System {systemID} needs to be replaced soon.
 
-    with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
-        server.login("121b33b64f36e0", "f7312395b9b74a")
-        server.sendmail(sender, receiver, message)
+Current Battery Life: {life}."""
+
+Water = f"""\
+Alert: The Water Source of System {systemID} is low.
+
+Current Water Level: {life}."""
+
+badData = f"""\
+Alert: Probe {pnum} of System {systemID} may have been tampered/damaged.
+
+Details: Probe {pnum} has been showing these values {badpval} against {goodpval} of Probe {pnum2}."""
+
+Reset = f"""\
+."""
+
+Stats = f"""\
+."""
+
+SystemInvites = f"""\
+."""
+
+def send_email(subject, body, recipient):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, 'alhq eqqw frgs dpfo')
+        smtp_server.sendmail(sender, recipient, msg.as_string())
+
+    print("please work :(")
