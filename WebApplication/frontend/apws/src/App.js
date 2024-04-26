ba95c74e527925b3a4400f85de4630154c0eb3c9
im-alexis <alexis.torres1802@gmail.com>
2024-04-26 00:17:55 -0500

    Reverting back changes for deployemt, going to isolate

--- WebApplication/frontend/apws/src/App.js
+++ WebApplication/frontend/apws/src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import Login from './components/login'
-import Landing from './components/landing';
-import CreateUser from './components/CreateUser';
-import Forgot from './components/Forgot';
-import Dashboard from './components/Dashboard';
-import Register from './components/Register';
-import NotFound from './components/errorPage';
-import Join from './components/join';
-import History from './components/History';
+import "./App.css";
+import Login from "./components/login";
+import Landing from "./components/landing";
+import CreateUser from "./components/CreateUser";
+import Forgot from "./components/Forgot";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import NotFound from "./components/errorPage";
+import Join from "./components/join";
+import History from "./components/History";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react';
-import Verify from './components/verify';
-import Reset_password from './components/reset_password';
-import Plant_Settings from './components/settings';
+import React from "react";
+import Verify from "./components/verify";
+import Reset_password from "./components/reset_password";
+import Plant_Settings from "./components/settings";
 // function App() {
 //   return (
 //     <div className="App">
@@ -36,25 +36,25 @@ import Plant_Settings from './components/settings';
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Landing />}></Route>
-              <Route path="/Login" element={<Login />}></Route>
-              <Route path="/Create-User" element={<CreateUser />}></Route>
-              <Route path="/Forgot" element={<Forgot />}></Route>
-              <Route path="/Dashboard" element={<Dashboard />}></Route>
-              <Route path="/verify" element={<Verify />}></Route>
-              <Route path="/reset_password" element={<Reset_password />}></Route>
-              <Route path="/Register" element={<Register />}></Route>
-              <Route path="/Join" element={<Join />}></Route>
-              <Route path="/Plant_Settings" element={<Plant_Settings />}></Route>
-              <Route path="/plantHistory" element={<History />}></Route>
-              <Route path="*" element={<NotFound />}>
-                  {/*<Route path="projects" element={<Projects />} />*/}
-                  {/*/!*{ <Route path="*" element={<NoPage />} />}*!/*/}
-              </Route>
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Create-User" element={<CreateUser />}></Route>
+        <Route path="/Forgot" element={<Forgot />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/verify" element={<Verify />}></Route>
+        <Route path="/reset_password" element={<Reset_password />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
+        <Route path="/Join" element={<Join />}></Route>
+        <Route path="/Plant_Settings" element={<Plant_Settings />}></Route>
+        <Route path="/plantHistory" element={<History />}></Route>
+        <Route path="*" element={<NotFound />}>
+          {/*<Route path="projects" element={<Projects />} />*/}
+          {/*/!*{ <Route path="*" element={<NoPage />} />}*!/*/}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
